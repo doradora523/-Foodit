12794069fdabc8a122d88919c945f418b6e03e16
Tory <88531407+1two13@users.noreply.github.com>
2023-06-24 00:40:48 +0900

    design: app css 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './styles/App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div class="flex  h-screen items-center justify-center bg-backgroundColor">
+      <div className="App" class="bg-white w-[390px] h-[844px] m-auto"></div>
+    </div>
+  );
 }
 
 export default App;
