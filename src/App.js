23302fb70c58da4cd439c4fed91e475f270a2117
Tory <88531407+1two13@users.noreply.github.com>
2023-06-25 01:48:25 +0900

    design: 와이어프레임 변경에 의한 css 설정
    top 47px, bottom 34px 고정

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './styles/App.css';
 
 function App() {
   return (
-    <div class="flex  h-screen items-center justify-center bg-backgroundColor">
-      <div className="App" class="bg-white w-[390px] h-[844px] m-auto"></div>
+    <div className="App">
+      <div className="page-wrapper"></div>
     </div>
   );
 }
