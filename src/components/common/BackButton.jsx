a15c0a8ed46ae1afa7060294fb4f20394f0e5cac
Tory <88531407+1two13@users.noreply.github.com>
2023-06-25 03:09:28 +0900

    rename: 경로 이동

--- src/components/common/BackButton.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-function BackButton() {
-  // TODO 추후 이미지나 만들어진 아이콘으로 수정 예정
-  return <button class="border w-[44px] h-[44px]">{'<'}</button>;
-}
-
-export default BackButton;
--- src/components/registerLocationPage/NearLoacation.jsx
+++ src/components/registerLocationPage/NearLoacation.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function NearLoacation({ location }) {
-  return <div class="flex w-[360px] py-[2%] border-b-[0.5px] items-center">{location}</div>;
+  return <div class="w-[360px] h-[42px] border-b-[0.5px] pb-[13px]">{location}</div>;
 }
 
 export default NearLoacation;
