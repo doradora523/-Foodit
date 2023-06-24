0df06fc22b193965a2b225a814b862f9abdd49b5
Tory <88531407+1two13@users.noreply.github.com>
2023-06-25 03:49:40 +0900

    design: gap 추가

--- src/pages/RegisterLocationPage.jsx
+++ src/pages/RegisterLocationPage.jsx
@@ -12,7 +12,7 @@ function RegisterLocationPage() {
 
       <div class="flex items-center h-[29px] my-[20px] ml-[15px] font-semibold text-[12px]">{NEAR_LOCATION}</div>
 
-      <div class="flex flex-col items-center h-[630px] overflow-scroll ">
+      <div class="flex flex-col items-center h-[630px] overflow-scroll gap-[13px]">
         <NearLoacation location={'서울특별시 서초구 역삼동'} />
         <NearLoacation location={'서울특별시 서초구 역삼동'} />
         <NearLoacation location={'서울특별시 서초구 역삼동'} />
