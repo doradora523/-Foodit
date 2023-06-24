f3b7f916f59ddbca4d3a4134247dff505d132e7d
Tory <88531407+1two13@users.noreply.github.com>
2023-06-24 12:58:19 +0900

    refactor: 검색창, 버튼 공통 컴포넌트로 분리

---
+++ src/components/common/BackButton.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function BackButton() {
+  // TODO 추후 이미지나 만들어진 아이콘으로 수정 예정
+  return <button class="border w-[44px] h-[44px]">{'<'}</button>;
+}
+
+export default BackButton;
--- src/pages/RegisterLocationPage.jsx
+++ src/pages/RegisterLocationPage.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import NearLoacation from '../components/registerLocationPage/NearLoacation';
 import { SEARCH_LOCATION, NEAR_LOCATION } from '../static/constants';
+import SearchBar from '../components/common/SearchBar';
 
 function RegisterLocationPage() {
   return (
     <div class="flex flex-col border h-[100%]">
-      <div class="border flex mt-[40px]">
-        <button class="border w-[44px] h-[44px]">{'<'}</button>
-        <input
-          placeholder={SEARCH_LOCATION}
-          class="border w-[100%] px-[10px] rounded-[5px] bg-lightGray placeholder-gray text-[13px]"
-        />
-      </div>
+      <SearchBar placeholder={SEARCH_LOCATION} />
 
       <div class="border pl-[16px] my-[25px]">{NEAR_LOCATION}</div>
 
