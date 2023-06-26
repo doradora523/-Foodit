0d1e61c0165bc636584a9d82f176678aa7f84264
Tory <88531407+1two13@users.noreply.github.com>
2023-06-26 14:11:43 +0900

    style: 사용하지 않는 주석 제거

--- src/components/common/navBar/SearchBar.jsx
+++ src/components/common/navBar/SearchBar.jsx
@@ -7,7 +7,6 @@ function SearchBar({ placeholder }) {
   return (
     <div class="flex h-[44px] mt-[47px]">
       <BackButton onClickHandler={moveToPermissionPage} />
-      {/* TODO: placeholder 색상 반응형으로 */}
       <input
         placeholder={placeholder}
         class="w-[100%] mr-[15px] px-[27px] rounded-[5px] bg-lightGray placeholder-deepGray text-[13px]"
