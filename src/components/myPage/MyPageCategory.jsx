129787b79e490951759dd77389a1dbdbf8033549
Tory <88531407+1two13@users.noreply.github.com>
2023-06-27 02:25:52 +0900

    design: 와이어프레임 수정되어 코드 수정

--- src/components/myPage/MyPageCategory.jsx
+++ src/components/myPage/MyPageCategory.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { TEMPORARY_SRC } from '../../static/constants';
-
 const Button = styled.button`
   color: ${(props) => props.color || '#6B6B6B'};
 `;
@@ -10,7 +8,9 @@ const Button = styled.button`
 function MyPageCategory({ name, color }) {
   return (
     <div class="flex mb-[29px]">
-      <img alt="myPageCategory" src={TEMPORARY_SRC} class="w-[30px] h-[30px] ml-[15px] mr-[26px]" />
+      <div class="w-[30px] h-[30px] ml-[15px] mr-[26px] bg-gray">
+        {/* <img alt="myPageCategory" src={TEMPORARY_SRC} /> */}
+      </div>
       <Button className="text-[16px] text-medium" color={color}>
         {name}
       </Button>
