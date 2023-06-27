5312721d5d6939510dc5c17e914a350996c3bc1c
Tory <88531407+1two13@users.noreply.github.com>
2023-06-27 17:11:11 +0900

    design: 글 작성 페이지 CSS 수정

--- src/components/writingPage/Input.jsx
+++ src/components/writingPage/Input.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Input({ placeholder }) {
   return (
-    <div class="py-[15px] mx-[15px] text-[13px] text-darkGray border-b-[0.5px] border-gray">
-      <input placeholder={placeholder} />
+    <div class="mx-[15px] text-[13px] text-darkGray border-b-[0.5px] border-gray">
+      <input placeholder={placeholder} class="h-[54px] w-[100%] outline-0" />
     </div>
   );
 }
