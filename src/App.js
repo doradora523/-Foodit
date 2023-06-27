0f85a25bc07c474acef7c74332c1328dd579362a
Tory <88531407+1two13@users.noreply.github.com>
2023-06-28 00:53:49 +0900

    feat: 라우터 사용을 위한 코드 작성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './styles/App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import WritingPage from './pages/WritingPage';
+import CategoryListPage from './pages/CategoryListPage';
+import PostsPage from './pages/PostsPage';
 
 function App() {
   return (
     <div className="App">
-      <div className="page-wrapper"></div>
+      <div className="page-wrapper">
+        <Routes>
+          <Route path="/writingPage" element={<WritingPage />} />
+          <Route path="/categoryListPage" element={<CategoryListPage />} />
+          <Route path="/postsPage" element={<PostsPage />} />
+        </Routes>
+      </div>
     </div>
   );
 }
