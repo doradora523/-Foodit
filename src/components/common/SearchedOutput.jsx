9c578e3681a1e13f16d161c6bd4cc0858d3309b7
Tory <88531407+1two13@users.noreply.github.com>
2023-06-29 17:50:48 +0900

    refactor: 하드코딩된 값 props로 변경

--- src/components/common/SearchedOutput.jsx
+++ src/components/common/SearchedOutput.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BEFORE, TEMPORARY_SRC, WON } from '../../static/constants';
+import { BEFORE, WON } from '../../static/constants';
 
-// TODO: 하드코딩되어 있는 값들 모두 수정 필요
-function SearchedOutput() {
+function SearchedOutput({ src, name, location, price }) {
+  // TODO: price에 콤마 추가해야할 수도
   return (
     <div className="flex">
-      <img alt={'searchedImage'} src={TEMPORARY_SRC} className="w-[100px] rounded-[10px]" />
+      <img alt={'searchedImage'} src={src} className="w-[100px] rounded-[10px]" />
       <div className="ml-[15px] flex flex-col justify-between">
         <div>
-          <div className="text-[13px] font-semibold">{'프로틴 바/ 다크 초코씨솔트&카라멜넛, 마이프로틴 바'}</div>
-          <div className="text-[10px] text-smokeGray">{`${'서울시 서초구 서초대로'} | ${'20분'} ${BEFORE}`}</div>
+          <div className="text-[13px] font-semibold">{name}</div>
+          <div className="text-[10px] text-smokeGray">{location}</div>
         </div>
-        <div className="text-[16px] font-bold">{`${'20,000'}${WON}`}</div>
+        <div className="text-[16px] font-bold">{`${price}${WON}`}</div>
       </div>
     </div>
   );
--- src/components/common/SearchedOutputLists.jsx
+++ src/components/common/SearchedOutputLists.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import SearchedOutput from './SearchedOutput';
 
-function SearchedOutputList() {
+import { TEMPORARY_SRC } from '../../static/constants';
+
+function SearchedOutputList({ searchedOutputList }) {
+  // TODO: 무한 스크롤 구현
   return (
     <div className="flex flex-col mx-[15px] mb-[27px] overflow-scroll gap-[20px] h-[520px]">
-      <SearchedOutput />
-      <SearchedOutput />
-      <SearchedOutput />
-      <SearchedOutput />
-      <SearchedOutput />
-      <SearchedOutput />
-      <SearchedOutput />
-      <SearchedOutput />
+      {searchedOutputList &&
+        searchedOutputList.map((el) => (
+          <SearchedOutput
+            key={el.id}
+            src={TEMPORARY_SRC}
+            name={el.name}
+            location={el.location}
+            price={Number(el.price).toLocaleString()}
+          />
+        ))}
     </div>
   );
 }
